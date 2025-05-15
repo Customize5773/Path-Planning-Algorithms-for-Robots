8e6883d2fc9a6cea5de37b8227857f50ac8b58aa
Ras Rizkov <132064583+Customize5773@users.noreply.github.com>
2025-05-15 22:54:08 +0700

    Create pid_controller.cpp

---
+++ src/pid_controller.cpp
@@ -0,0 +1,14 @@
+#include "pid_controller.h"
+
+PIDController::PIDController(float kp, float ki, float kd)
+    : _kp(kp), _ki(ki), _kd(kd),
+      _prevError(0), _integral(0) {}
+
+float PIDController::compute(float setpoint, float measured, float dt) {
+    float error = setpoint - measured;
+    _integral += error * dt;
+    float derivative = (error - _prevError) / dt;
+    _prevError = error;
+
+    return _kp * error + _ki * _integral + _kd * derivative;
+}
